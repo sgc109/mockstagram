89367e8714095d28673a95496b580692d7fc638f
Wood Hwang <sh.hwang@bucketplace.net>
2024-11-28 02:27:38 +0900

    initialize project

---
+++ content/batch/src/test/kotlin/sean/hwang/batch/BatchApplicationTests.kt
@@ -0,0 +1,12 @@
+package sean.hwang.batch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
---
+++ content/consumer/src/test/kotlin/sean/hwang/consumer/ConsumerApplicationTests.kt
@@ -0,0 +1,12 @@
+package sean.hwang.consumer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
