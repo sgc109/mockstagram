7a3c435a003e0d686e08d0684785acc475c94b4a
Wood Hwang <sh.hwang@bucketplace.net>
2024-12-01 18:09:51 +0900

    feat: added MongoDB on docker-compose.yml

---
+++ init/mongodb/mongo-init.js
@@ -0,0 +1,22 @@
+db = connect("mongodb://root:root@localhost:27017/admin");
+
+// 사용자 계정 목록
+const users = [
+    { username: "comment_user", password: "comment_pw", database: "comment" },
+    { username: "content_user", password: "content_pw", database: "content" },
+    { username: "reaction_user", password: "reaction_pw", database: "reaction" },
+    { username: "notification_user", password: "notification_pw", database: "notification" },
+    { username: "push_user", password: "push_pw", database: "push" },
+    { username: "user_user", password: "user_pw", database: "user" },
+];
+
+// 사용자 생성 및 권한 부여
+users.forEach(({ username, password, database }) => {
+    db = db.getSiblingDB(database); // 대상 데이터베이스
+    db.createUser({
+        user: username,
+        pwd: password,
+        roles: [{ role: "readWrite", db: database }],
+    });
+    print(`Created user ${username} for database ${database}`);
+});
--- run-only-infra.sh
+++ run-only-infra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose up --build mysql redis kafka
+docker-compose up --build mysql redis kafka mongodb
