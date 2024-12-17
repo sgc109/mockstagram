f3db2be4ccae747fabeb92452829fd3c8459c09d
Wood Hwang <sh.hwang@bucketplace.net>
2024-12-17 22:48:11 +0900

    feat: (reaction-api) add reaction-api on docker-compose.yml and build-all-gradle-projects.sh

--- build-all-gradle-projects.sh
+++ build-all-gradle-projects.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Gradle 프로젝트들이 있는 서브 디렉터리들
-subdirs=("content" "user")  # 쉼표 뒤 공백 제거
+subdirs=("content" "user" "reaction")  # 쉼표 뒤 공백 제거
 
-# 각 서브 디렉터리에서 jibDockerBuild 작업을 병렬로 실행
+# Execute jibDockerBuild task for each subdirectory concurrently
 for dir in "${subdirs[@]}"; do
   (cd "$dir" && ./gradlew jibDockerBuild) &
 done
 
-# 모든 병렬 작업이 끝날 때까지 기다리기
+# Wait for all jibDockerBuild tasks to complete
 wait
