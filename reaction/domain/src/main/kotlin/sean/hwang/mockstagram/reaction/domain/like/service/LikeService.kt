546dcaaa719c68f07f3478ab196c71bc8d05aa0e
Wood Hwang <sh.hwang@bucketplace.net>
2024-12-29 02:20:24 +0900

    feat: (reaction-domain) apply outbox pattern for like&unlike

--- reaction/domain/src/main/kotlin/sean/hwang/mockstagram/reaction/domain/like/service/LikeService.kt
+++ reaction/domain/src/main/kotlin/sean/hwang/mockstagram/reaction/domain/like/service/LikeService.kt
@@ -1,10 +1,12 @@
 package sean.hwang.mockstagram.reaction.domain.like.service
 
+import jakarta.persistence.EntityManager
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import sean.hwang.mockstagram.reaction.domain.like.dto.LikeTarget
 import sean.hwang.mockstagram.reaction.domain.like.entity.Like
 import sean.hwang.mockstagram.reaction.domain.like.entity.LikeCounter
+import sean.hwang.mockstagram.reaction.domain.like.entity.LikeOutbox
 import sean.hwang.mockstagram.reaction.domain.like.entity.LikeTargetType
 import sean.hwang.mockstagram.reaction.domain.like.repository.LikeCounterRepository
 import sean.hwang.mockstagram.reaction.domain.like.repository.LikeRepository
@@ -14,6 +16,7 @@ import sean.hwang.mockstagram.reaction.domain.like.repository.LikeRepository
 class LikeService(
     private val likeRepository: LikeRepository,
     private val likeCounterRepository: LikeCounterRepository,
+    private val entityManager: EntityManager,
 ) {
     @Transactional
     fun createLike(like: Like): Like {
@@ -21,7 +24,9 @@ class LikeService(
             ?: LikeCounter(targetId = like.targetId, targetType = like.targetType, count = 0)
 
         likeCounterRepository.save(likeCounter.increment())
-        return likeRepository.save(like)
+        return likeRepository.save(like).also {
+            publishLikeEvent(aggregate = it, type = LikeOutbox.EventType.CREATED)
+        }
     }
 
     @Transactional
@@ -40,7 +45,11 @@ class LikeService(
 
         likeCounterRepository.save(likeCounter.decrement())
 
-        likeRepository.delete(like)
+        like.also {
+            likeRepository.delete(it)
+        }.also {
+            publishLikeEvent(aggregate = it, type = LikeOutbox.EventType.DELETED)
+        }
     }
 
     fun batchGetLikeCounts(likeTargets: List<LikeTarget>): Map<LikeTarget, Long> {
@@ -64,5 +73,15 @@ class LikeService(
 
     private fun filterLikeTargets(likeTargets: List<LikeTarget>, targetType: LikeTargetType): List<String> {
         return likeTargets.filter { it.targetType == targetType }.map { it.targetId }
+    }
+
+    private fun publishLikeEvent(aggregate: Like, type: LikeOutbox.EventType) {
+        entityManager.persist(
+            LikeOutbox(
+                aggregateId = aggregate.id,
+                type = type,
+                payload = aggregate,
+            )
+        )
     }
 }
