2b569fe962d2a5203fd6ed194b1bab08e8eff6ff
Wood Hwang <sh.hwang@bucketplace.net>
2024-12-31 21:29:45 +0900

    feat: update build-all-gradle-projects.sh

--- build-all-gradle-projects.sh
+++ build-all-gradle-projects.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # subdirectories to search for Gradle projects
-subdirs=("content" "comment" "reaction" "user")
+subdirs=("content" "comment" "reaction" "user" "notification")
 
 # Execute jibDockerBuild task for each subdirectory concurrently
 for dir in "${subdirs[@]}"; do
